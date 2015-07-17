f734995d3d0339f430f0f18ecc2910c7306fcbe4
Joe Bergantine <jbergantine@gmail.com>
2015-07-17 08:31:02 -0600

    updated description in setup

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from setuptools import setup
 setup(
     name='sampen',
     version='0.0.1',
-    description='A Python module to calculate Sample Entropy (SampEn).',
+    description='A Python module to calculate Sample Entropy (SampEn) of a'
+                ' time series.',
     url='http://www.physionet.org/physiotools/sampen',
     download_url='https://github.com/jbergantine/sampen/tarball/0.0.1/',
     author='Doug Lake',
