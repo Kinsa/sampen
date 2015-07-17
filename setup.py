b85f85ea5105f90f9301153e3acf11f853fc2253
Joe Bergantine <jbergantine@gmail.com>
2015-07-17 09:19:30 -0600

    tested and verified works in py 3.4, went ahead and put in full git clone url

--- setup.py
+++ setup.py
@@ -25,5 +25,7 @@ setup(
         'Topic :: Scientific/Engineering :: Mathematics',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
     ]
 )
