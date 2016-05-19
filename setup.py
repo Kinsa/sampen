6f48ef4ee51515d2bc00deebc1fb84515686bbde
Joe Bergantine <jbergantine@gmail.com>
2016-05-19 13:01:59 -0600

    Bug fixes and modular improvements
    - Moved functions out of the init method and into separate modules
    - Revised normalize data to more closely match the original C script;
      fixing some looping and mathematical bugs
    - Close #1

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 
 setup(
     name='sampen',
-    version='0.0.10',
+    version='0.0.11',
     description='A Python module to calculate Sample Entropy (SampEn) of a'
                 ' time series.',
     url='http://www.physionet.org/physiotools/sampen',
-    download_url='https://github.com/jbergantine/sampen/tarball/0.0.10/',
+    download_url='https://github.com/jbergantine/sampen/tarball/0.0.11/',
     author='Doug Lake',
     author_email='dlake@virginia.edu',
     maintainer='Joe Bergantine',
@@ -18,6 +18,7 @@ setup(
         'numpy>=1.9.2',
     ],
     keywords=['sample entropy', 'sampen'],
+    test_suite="tests.test_sampen",
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
@@ -27,7 +28,10 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Topic :: Scientific/Engineering :: Mathematics',
