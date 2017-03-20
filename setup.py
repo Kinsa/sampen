b2e969fa64eefcb25e8902f3836e189c5836af4b
Joe Bergantine <jbergantine@gmail.com>
2017-03-20 10:25:06 -0600

    version bump

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 
 setup(
     name='sampen',
-    version='0.0.14',
+    version='0.0.15',
     description='A Python module to calculate an estimation of the '
                 'Sample Entropy of a time series.',
     long_description=open("README.rst").read(),
     url='https://readthedocs.org/projects/sampen/',
-    download_url='https://github.com/jbergantine/sampen/tarball/0.0.14/',
+    download_url='https://github.com/jbergantine/sampen/tarball/0.0.15/',
     author='Joe Bergantine',
     author_email='joe@kinsa.us',
     maintainer='Joe Bergantine',
